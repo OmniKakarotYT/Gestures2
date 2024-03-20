35f3e8479cad05625520413ea97cd5c7e109fd3e
Omni_Kakarot_YT <134367842+OmniKakarotYT@users.noreply.github.com>
2024-03-20 16:41:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//press start!
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+
+camera = document.getElementById("camera");
+
+
+Webcam.attach('#camera');
+
+//snapped
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+//consoled
+console.log('ml5 version:', ml5.version);
+//classified
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/VImY7gqFMf/model.json',modelLoaded);
+
+//defined modelloaded
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
